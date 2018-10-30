c9945a97aeb0ccf8c0056b75e27b38d21ad6929d
Justin Davis <justin_w_davis@yahoo.com>
2018-10-30 15:19:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,12 @@
 function getIt() {
-  $('p').on('click', function(){
+  $( 'p' ).on( 'click', function() {
     alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $( 'img' ).on( 'load', function() {
+    $( 'img' ).addClass( 'tasty' )
   })
 }
 
