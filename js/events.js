ae26f04860175d892378748c57f8b89e012d240b
Justin Davis <justin_w_davis@yahoo.com>
2018-10-30 15:14:01 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,11 +1,11 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
 
 $(document).ready(function(){
 
-  function getIt() {
-    $('p').on('click', function(){
-      alert('Hey!')
-    })
-  }
+
 
 });
